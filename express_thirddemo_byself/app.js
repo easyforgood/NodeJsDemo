fcfc91dd5fe62944c19b4e0a055d11b36ff902a3
Siplexy <easyforgood@hotmail.com>
2015-02-02 09:30:08 +0800

    third:  Post

--- express_thirddemo_byself/app.js
+++ express_thirddemo_byself/app.js
@@ -1,6 +1,8 @@
 var express=require("express");
+var bodyParser=require("body-parser");
 var app=new express();
 var logger=require("./logger");
+
 var ejs=require("ejs");
 //app.engine('.html', ejs.__express);
 app.set('view engine', 'html');
@@ -8,9 +10,16 @@ app.use(logger);
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/views'));
 
+app.use(bodyParser.urlencoded());
 app.get("/user",function(req,res){
     res.write("This is a test\n");
     res.write("the input param is  " + req.query.name);
+    res.end();
+
+});
+app.post("/user",function(req,res){
+    res.write("This is a test\n");
+    res.write("the input param is  " + req.body.name);
     res.end();
 
 });
